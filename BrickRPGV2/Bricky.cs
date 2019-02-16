ab5bab1ec9a520449696a30fba115eea245df8e6
jfmekker <jfm8746@rit.edu>
2019-02-16 18:27:32 -0500

    Bricky Changes

--- BrickRPGV2/Bricky.cs
+++ BrickRPGV2/Bricky.cs
@@ -1,36 +1,49 @@
 ﻿using System;
-
 using System.Collections.Generic;
-
 using System.Linq;
-
 using System.Text;
-
 using System.Threading.Tasks;
-
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-namespace Characters
-
+namespace BrickRPGV2
 {
-
     class Bricky
-
     {
-        public Texture2D playerTexture;
-        public Vector2 Position;
-        public bool Active;
-        public int Health;
-        public void Init()
-        {
+        // Main attributes
+        public AnimatedSprite2D TopDownPawn; // change to Entity TODO
+        public AnimatedSprite2D InsidePawn; // change to Entity TODO
+        public bool IsInside;
+        public float GPA;
+        public float Stamina;
 
+        // Regen values
+        public float GPA_regen = 0f;
+        public float Stamina_regen = 0.25f; //per second
+
+        // Weapons
+        int coffees;
+        int energyDrinks;
+        bool keyboard; // or pencil(?)
+
+        public Bricky()
+        {
+            //init TopDownPawn
+            //init InsidePawn
+            GPA = 4.0f;
+            Stamina = 100;
+            IsInside = true;
+            coffees = 0f;
+            energyDrinks = 0f;
+            keyboard = false;
         }
+
         public void Update()
         {
 
         }
+
         public void Draw()
         {
 
