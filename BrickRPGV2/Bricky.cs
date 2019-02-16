3645979b8204b911902e36a520a3b51e6cdb894f
Sean Bashaw <seanbshw@gmail.com>
2019-02-16 16:50:09 -0500

    Added Bricky
    Derpy Derp

---
+++ BrickRPGV2/Bricky.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Characters
+
+{
+
+    class Bricky
+
+    {
+        public Texture2D playerTexture;
+        public Vector2 Position;
+        public bool Active;
+        public int Health;
+        public void Init()
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+        public void Draw()
+        {
+
+        }
+    }
+
+}
