bf6a3126c62c5d91375958373f0f55abdad5a2c9
Sean Bashaw <seanbshw@gmail.com>
2019-02-16 21:43:55 -0500

    Started Combat System
    Combat System Started

---
+++ BrickRPGV2/Combat.cs
@@ -0,0 +1,16 @@
+﻿using Characters;
+namespace BrickRPGV2
+{
+    interface Weapon
+    {
+        int attack(Entity e);
+        void drainMana(Entity e);
+        bool hitOrMiss(Entity e);
+    }
+    interface Shield
+    {
+        int defend(Entity e);
+        bool blockChance(Entity e);
+        void breakDown();
+    }
+}
